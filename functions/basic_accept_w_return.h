cd5505d6ec1a31df1df7d8f6bd49d74579528a52
IamTV50 <95366061+IamTV50@users.noreply.github.com>
2024-03-11 22:29:49 +0100

    first commit

---
+++ functions/basic_accept_w_return.h
@@ -0,0 +1,63 @@
+#ifndef PLAYING_WITH_WINSOCK2_BASIC_ACCEPT_W_RETURN_H
+#define PLAYING_WITH_WINSOCK2_BASIC_ACCEPT_W_RETURN_H
+
+#endif //PLAYING_WITH_WINSOCK2_BASIC_ACCEPT_W_RETURN_H
+
+#include <iostream>
+#include <winsock2.h> //add "target_link_libraries(${CMAKE_PROJECT_NAME} ws2_32)" to CMakeLists.txt
+#include <ws2tcpip.h>
+
+void basic_accept_w_return(){
+    WSADATA wsaData = {0};
+    int iResult = 0;
+    sockaddr_in service; // The socket address to be passed to bind
+
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+
+    SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if (listenSocket == INVALID_SOCKET){
+        wprintf(L"socket function failed with error = %d\n", WSAGetLastError() );
+    }
+
+    //define sockaddr structure (https://learn.microsoft.com/en-us/windows/win32/winsock/sockaddr-2)
+    service.sin_family = AF_INET;
+    service.sin_addr.s_addr = inet_addr("127.0.0.1");
+    service.sin_port = htons(9999);
+
+    //bind the socket
+    iResult = bind(listenSocket, (SOCKADDR *) &service, sizeof(service));
+    if (iResult == SOCKET_ERROR){
+        wprintf(L"bind function failed with error = %d\n", WSAGetLastError() );
+    }
+
+    //listen for incoming connection requests on created socket
+    if(listen(listenSocket, SOMAXCONN) == SOCKET_ERROR){
+        wprintf(L"listen function failed with error: %d\n", WSAGetLastError());
+    }
+    wprintf(L"listening on socket....\n");
+
+    // Create a SOCKET for accepting incoming requests.
+    SOCKET acceptSocket;
+    wprintf(L"Waiting for client to connect...\n");
+
+    //accepts the connection
+    acceptSocket = accept(listenSocket, nullptr, nullptr);
+    if (acceptSocket == INVALID_SOCKET) {
+        wprintf(L"accept failed with error: %ld\n", WSAGetLastError());
+        closesocket(listenSocket);
+        WSACleanup();
+    }
+
+    wprintf(L"client connected... \n");
+    //todo - find a way to return something to client
+
+
+    //closing sockets
+    closesocket(listenSocket);
+    closesocket(acceptSocket);
+
+    WSACleanup();
+}
+
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "functions/basic_accept_wo_return.h"
+#include "functions/basic_accept_w_return.h"
+
+int main() {
+    basic_accept_wo_return();
+
+    return 0;
+}
